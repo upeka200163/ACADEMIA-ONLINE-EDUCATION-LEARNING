662c46a4865cbc66c6d72f26e535a79c4fb6db6f
Upeka Mahanama <upekamahanama@gmail.com>
2026-05-15 16:17:15 +0530

    Initial commit

---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Heading from "../../common/heading/Heading"
+import "./Hero.css"
+
+const Hero = () => {
+  return (
+    <>
+      <section className='hero'>
+        <div className='container'>
+          <div className='row'>
+            <Heading subtitle='WELCOME TO ACADEMIA' title='Best Online Education Expertise' />
+            <p>Join us to easily access world-class knowledge and skills from anywhere, guided by expert mentorship, to reach your highest potential. This is the best opportunity to turn your future dreams into reality.</p>
+            <div className='button'>
+              <button className='primary-btn'>
+                GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+              <button>
+                VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className='margin'></div>
+    </>
+  )
+}
+
+export default Hero
---
+++ src/dummydata.js
@@ -0,0 +1,415 @@
+export const homeAbout = [
+  {
+    id: 1,
+    cover: "https://img.icons8.com/dotty/80/000000/storytelling.png",
+    title: "Online Courses",
+    desc:  "Access a wide variety of high-quality courses designed by industry experts to help you master new professional skills.",
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/diploma.png",
+    title: "Earn A Certificates",
+    desc:  "Validate your hard work and achievements with recognized digital certificates that will significantly boost your professional career profile.",
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/athlete.png",
+    title: "Learn with Expert",
+    desc:  "Get direct mentorship and insights from world-class professionals who bring years of real-world experience to your learning journey.",
+  },
+]
+export const awrapper = [
+  {
+    cover: "https://img.icons8.com/external-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto/80/ffffff/external-graduation-education-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto.png",
+    data: "3,000",
+    title: "SUCCESS STORIES",
+  },
+
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/athlete.png",
+    data: "320",
+    title: "TRUSTED TUTORS",
+  },
+  {
+    cover: "https://img.icons8.com/external-outline-icons-maxicons/80/ffffff/external-calender-insurance-outline-outline-icons-maxicons.png",
+    data: "1,000",
+    title: "SCHEDULES",
+  },
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/macbook-idea--v3.png",
+    data: "587",
+    title: "COURSES",
+  },
+]
+export const coursesCard = [
+  {
+    id: 1,
+    cover: "../images/courses/c1.png",
+    coursesName: "Introducing to Software Engineering",
+    courTeacher: [
+      {
+        dcover: "./images/back1.webp",
+        name: "by Anne Sharma",
+        totalTime: "50 lectures (190 hrs)",
+      },
+    ],
+    priceAll: "$100 All Course",
+    pricePer: "$15 per month",
+  },
+  {
+    id: 2,
+    cover: "../images/courses/c2.png",
+    coursesName: "Enhancing Adobe Photoshop CC 2020 Skills",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Rumi Gurung",
+        totalTime: "30 lectures (125 hrs)",
+      },
+    ],
+    priceAll: "$200 All Course",
+    pricePer: "$25 per month",
+  },
+  {
+    id: 3,
+    cover: "../images/courses/c3.png",
+    coursesName: "HTML, CSS, and Javascript for Web Developers",
+    courTeacher: [
+      {
+        dcover: "./images/back2.webp",
+        name: "by Saroj Nepal",
+        totalTime: "50 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "$50 All Course",
+    pricePer: "$5 per month",
+  },
+  {
+    id: 4,
+    cover: "../images/courses/c4.png",
+    coursesName: "Introducing to Programming with WordPress",
+    courTeacher: [
+      {
+        dcover: "./images/back3.webp",
+        name: "by Prabin Thapa Mager",
+        totalTime: "20 lectures (20 hrs)",
+      },
+    ],
+    priceAll: "$30 All Course",
+    pricePer: "$3 per month",
+  },
+  {
+    id: 5,
+    cover: "../images/courses/c5.png",
+    coursesName: "Introducing to Programming with ReactJS",
+    courTeacher: [
+      {
+        dcover: "./images/back4.webp",
+        name: "by Shanuli Bishowkaram",
+        totalTime: "100 lectures (150 hrs)",
+      },
+    ],
+    priceAll: "$300 All Course",
+    pricePer: "$30 per month",
+  },
+  {
+    id: 6,
+    cover: "../images/courses/c6.png",
+    coursesName: "Learn Frontend Programming Language",
+    courTeacher: [
+      {
+        dcover: "./images/back5.webp",
+        name: "by Thunil Mashowkaram",
+        totalTime: "200 lectures (300 hrs)",
+      },
+    ],
+    priceAll: "$500 All Course",
+    pricePer: "$80 per month",
+  },
+  {
+    id: 7,
+    cover: "../images/courses/c7.png",
+    coursesName: "Introducing to with HTML / CSS",
+    courTeacher: [
+      {
+        dcover: "./images/back6.webp",
+        name: "by Chavin Mendis",
+        totalTime: "20 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "$20 All Course",
+    pricePer: "$2 per month",
+  },
+  {
+    id: 8,
+    cover: "../images/courses/c8.png",
+    coursesName: "Introducing to with JAVA",
+    courTeacher: [
+      {
+        dcover: "./images/back7.webp",
+        name: "by Githrani Petter",
+        totalTime: "80 lectures (200 hrs)",
+      },
+    ],
+    priceAll: "$200 All Course",
+    pricePer: "$40 per month",
+  },
+  {
+    id: 9,
+    cover: "../images/courses/c9.png",
+    coursesName: "Introducing to with HTML / CSS / JAVASCRIPT",
+    courTeacher: [
+      {
+        dcover: "./images/back8.webp",
+        name: "by Teya Noyel",
+        totalTime: "50 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "$20 All Course",
+    pricePer: "$3 per month",
+  },
+]
+export const online = [
+  {
+    cover: "./images/courses/online/o1.png",
+    hoverCover: "./images/courses/online/o1.1.png",
+    courseName: "UI/UX Design Courses",
+    course: "25 Courses",
+  },
+  {
+    cover: "./images/courses/online/o2.png",
+    hoverCover: "./images/courses/online/o2.1.png",
+    courseName: "Art & Design",
+    course: "25 Courses",
+  },
+  {
+    cover: "./images/courses/online/o3.png",
+    hoverCover: "./images/courses/online/o3.1.png",
+    courseName: "Computer Science",
+    course: "10 Courses",
+  },
+  {
+    cover: "./images/courses/online/o4.png",
+    hoverCover: "./images/courses/online/o4.1.png",
+    courseName: "History & Archeologic",
+    course: "15 Courses",
+  },
+  {
+    cover: "./images/courses/online/o5.png",
+    hoverCover: "./images/courses/online/o5.1.png",
+    courseName: "Software Engineering",
+    course: "30 Courses",
+  },
+  {
+    cover: "./images/courses/online/o6.png",
+    hoverCover: "./images/courses/online/o6.1.png",
+    courseName: "Information Software",
+    course: "60 Courses",
+  },
+  {
+    cover: "./images/courses/online/o7.png",
+    hoverCover: "./images/courses/online/o7.1.png",
+    courseName: "Health & Fitness",
+    course: "10 Courses",
+  },
+  {
+    cover: "./images/courses/online/o8.png",
+    hoverCover: "./images/courses/online/o8.1.png",
+    courseName: "Marketing",
+    course: "30 Courses",
+  },
+  {
+    cover: "./images/courses/online/o9.png",
+    hoverCover: "./images/courses/online/o9.1.png",
+    courseName: "Graphic Design",
+    course: "80 Courses",
+  },
+  {
+    cover: "./images/courses/online/o10.png",
+    hoverCover: "./images/courses/online/o10.1.png",
+    courseName: "Music",
+    course: "120 Courses",
+  },
+  {
+    cover: "./images/courses/online/o11.png",
+    hoverCover: "./images/courses/online/o11.1.png",
+    courseName: "Business Administration",
+    course: "17 Courses",
+  },
+  {
+    cover: "./images/courses/online/o12.png",
+    hoverCover: "./images/courses/online/o12.1.png",
+    courseName: "Web Management",
+    course: "24 Courses",
+  },
+]
+export const team = [
+  {
+    cover: "./images/team/t1.webp",
+    name: "Ph.D Adrian Molises",
+    work: "WEB DEVELOPER AND LEAD \n INSTRUCTOR",
+  },
+  {
+    cover: "./images/team/t2.webp",
+    name: "Ph.D Arthur MaGregor",
+    work: "BUSINESS DEVELOPER AND LEAD \n INSTRUCTOR",
+  },
+  {
+    cover: "./images/team/t3.webp",
+    name: "Ph.D Anna Hanzen",
+    work: "SOFTWARE DEVELOPER AND LEAD \n INSTRUCTOR",
+  },
+  {
+    cover: "./images/team/t4.webp",
+    name: "Ph.D Brian Wooden",
+    work: "DESIGN DEVELOPER AND LEAD \n INSTRUCTOR",
+  },
+  {
+    cover: "./images/team/t5.webp",
+    name: "Ph.D Linda Perry",
+    work: "MUSIC PRODUCER AND LEAD \n INSTRUCTOR",
+  },
+  {
+    cover: "./images/team/t6.webp",
+    name: "Ph.D  Daniel Carter",
+    work: "MULTIMEDIA SPECIALIST AND LEAD \n INSTRUCTOR",
+  },
+  {
+    cover: "./images/team/t7.webp",
+    name: "Ph.D Harshi Manzen",
+    work: "HEALTH DEVELOPER AND LEAD \n INSTRUCTOR",
+  },
+  {
+    cover: "./images/team/t8.webp",
+    name: "Ph.Dr. Zahi Hawass",
+    work: "ARCHAEOLOGIC RESEARCHER AND LEAD \n INSTRUCTOR",
+  },
+
+]
+export const price = [
+  {
+    name: "BASIC PLAN",
+    price: "49K",
+    desc: "Get started with our essential learning tools, basic course access, and community support to begin your educational journey today.",
+  },
+  {
+    name: "BEGINNER PLAN",
+    price: "79K",
+    desc: "Unlock additional features, including interactive modules, weekly assignments, and direct feedback from instructors to accelerate your continuous personal growth.",
+  },
+  {
+    name: "PREMIUM PLAN",
+    price: "109k",
+    desc: "Enjoy unlimited access to all premium courses, personalized mentorship sessions, and exclusive materials designed for serious and dedicated learners.",
+  },
+  {
+    name: "ULTIMATE PLAN",
+    price: "149K",
+    desc: "Experience the complete educational package providing personal professional coaching, lifetime course updates, and globally recognized certificates for ultimate success.",
+  },
+]
+export const faq = [
+  {
+    title: "How to Enroll This Online Courses?",
+    desc: "You can easily enroll in any online course by clicking the registration button located on the specific course page. After creating your personal account and completing the secure payment process, you will immediately receive full access to all the necessary learning materials, assignments, and video lectures provided.",
+  },
+  {
+    title: "Do you provide certificates after completion?",
+    desc: "Yes, we provide officially recognized digital certificates upon the successful completion of every premium course. You can easily download or share these verified certificates directly on your professional social media profiles to showcase your newly acquired skills and educational achievements.",
+  },
+  {
+    title: "Can I access the courses on my mobile device?",
+    desc: "Absolutely, our entire online learning platform is fully optimized for mobile devices and tablets. You can seamlessly watch video lectures, complete assignments, and interact with your instructors from anywhere using your smartphone without losing any important course features or content.",
+  },
+  {
+    title: "What is your refund policy for premium courses?",
+    desc: "We offer a complete money-back guarantee within the first fourteen days of your initial purchase. If you are not entirely satisfied with the course material, simply contact our support team to receive a full refund without answering any complicated questions.",
+  },
+  {
+    title: "Are there any prerequisites for joining a course?",
+    desc: "Most of our foundational courses do not require any prior experience or specific technical knowledge. However, some advanced professional programs might suggest having basic familiarity with certain topics to help you better understand the complex concepts discussed during the lectures.",
+  },
+  {
+    title: "How can I contact my instructor for help?",
+    desc: "You can easily communicate with your instructors through our dedicated student dashboard messaging system or during scheduled live Q&A sessions. Our experts are always committed to providing timely and helpful guidance to ensure that you successfully complete your educational journey.",
+  },
+]
+export const blog = [
+  {
+    id: 1,
+    type: "admin",
+    date: "JAN. 18, 2026",
+    com: "3 COMMENTS ",
+    title: "Build your Dream Software & Engineering Career",
+    desc: "Master the essential principles of software architecture, coding standards, and modern development methodologies for career success.",
+    cover: "../images/blog/b1.webp",
+  },
+  {
+    id: 2,
+    type: "admin",
+    date: "API. 25, 2026",
+    com: "5 COMMENTS ",
+    title: "Discover Ancient Secrets of History and Archeology",
+    desc: "Deep dive into ancient civilizations and discover hidden historical facts through our latest archaeological research programs.",
+    cover: "../images/blog/b2.webp",
+  },
+  {
+    id: 3,
+    type: "user",
+    date: "MAY. 15, 2026",
+    com: "10 COMMENTS ",
+    title: "Effective Marketing Strategies for Online Business",
+    desc: "Master the essential digital marketing skills needed to grow your online business and reach global audiences.",
+    cover: "../images/blog/b3.webp",
+  },
+  {
+    id: 4,
+    type: "admin",
+    date: "MAR. 02, 2026",
+    com: "6 COMMENTS ",
+    title: "Essential Management Skills for Web Administrators",
+    desc: "Discover professional techniques for managing complex websites and ensuring high performance and security for all users.",
+    cover: "../images/blog/b4.webp",
+  },
+  {
+    id: 5,
+    type: "admin",
+    date: "FEB. 14, 2026",
+    com: "7 COMMENTS ",
+    title: "Exploring the Future of Computer Science World",
+    desc: "Understand the fundamental concepts of programming and how software technology is changing the future of world.",
+    cover: "../images/blog/b5.webp",
+  },
+  {
+    id: 6,
+    type: "user",
+    date: "MAR. 18, 2026",
+    com: "12 COMMENTS ",
+    title: "Mastering Digital Creativity and Modern Interface Design",
+    desc: "Learn how to create user-friendly interfaces and improve digital experiences through modern research and design.",
+    cover: "../images/blog/b6.webp",
+  },
+]
+export const testimonal = [
+  {
+    id: 1,
+    name: "ROGER SCOTT",
+    post: "MARKETING MANAGER",
+    desc: "The professional education I received helped me master strategic digital marketing and successfully grow our global brand presence online.",
+    cover: "./images/testo/t1.webp",
+  },
+  {
+    id: 2,
+    name: "THOMAS HARDY",
+    post: "SENIOR SOFTWARE ENGINEER",
+    desc: "Implementing complex software architectures became much easier after completing the advanced engineering courses provided by this excellent educational platform.",
+    cover: "./images/testo/t2.webp",
+  },
+  {
+    id: 3,
+    name: "JANE SMITH",
+    post: "HEALTH DIRECTOR",
+    desc: "Developing effective health policies and managing hospital resources efficiently was possible due to the comprehensive leadership training provided here.",
+    cover: "./images/testo/t3.webp",
+  },
+]
